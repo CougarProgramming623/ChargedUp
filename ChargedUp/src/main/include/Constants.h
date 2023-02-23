1b5da50f0c668d917df0f2b966cafd1751b182c7
ashwinpotluri <ashwinpotluri01@gmail.com>
2023-02-23 18:48:14 -0500

    creating button ids for button board

--- ChargedUp/src/main/include/Constants.h
+++ ChargedUp/src/main/include/Constants.h
@@ -3,8 +3,6 @@
 // the WPILib BSD license file in the root directory of this project.
 
 #pragma once
-#include <math.h>
-
 
 /**
  * The Constants header provides a convenient place for teams to hold robot-wide
@@ -16,50 +14,70 @@
  * they are needed.
  */
 
-#define ROBOT_WHEELBASE
+namespace OperatorConstants {
 
-#ifdef ROBOT_WHEELBASE
+constexpr int kDriverControllerPort = 0;
 
+#define TELE_NUKE 9
+#define UP_DOWN_JOYSTICK 2
+#define RELEASE_BUTTON 12
+#define FEED_BUTTON 13
 
-    #define DRIVE_REDUCTION ((14.0 / 50.0) * (27.0 / 17.0) * (15.0 / 45.0))
-    #define STEER_REDUCTION ((14.0 /50.0) * (10.0 / 60.0))
-    #define WHEEL_DIAMETER 0.10033
-    #define DRIVETRAIN_TRACKWIDTH_METERS 0.5644
-    
-    #define STEER_ENCODER_POSITION_CONSTANT (2.0 * M_PI / 2048 * STEER_REDUCTION)
-    #define STEER_ENCODER_VELOCITY_CONSTANT (STEER_ENCODER_POSITION_CONSTANT * 10.0)
+#define ROBOT_SABER
+//#define ROBOT_WBOT
 
-    #define ENCODER_RESET_MAX_ANGULAR_VELOCITY (Deg2Rad(0.5))
-    #define ENCODER_RESET_ITERATIONS 500
-
-    #define DRIVE_ENCODER_POSITION_CONSTANT (M_PI * WHEEL_DIAMETER * DRIVE_REDUCTION / 2048)
-    #define DRIVE_ENCODER_VELOCITY_CONSTANT (DRIVE_ENCODER_POSITION_CONSTANT * 10)
-
-    /**
-     * The front-to-back distance between the drivetrain wheels.
-     *
-     * Should be measured from center to center.
-     */
-    #define DRIVETRAIN_WHEELBASE_METERS 0.666
-    #define ENCODER_VOLTAGE_TO_DEGREE (360/4.809)
-
-    #define FRONT_LEFT_MODULE_DRIVE_MOTOR 32
-    #define FRONT_LEFT_MODULE_STEER_MOTOR 35
-    //#define FRONT_LEFT_MODULE_STEER_OFFSET -Deg2Rad(0.0)
-    #define FRONT_LEFT_MODULE_ENCODER_PORT 3
-
-    #define FRONT_RIGHT_MODULE_DRIVE_MOTOR 37
-    #define FRONT_RIGHT_MODULE_STEER_MOTOR 33
-    //#define FRONT_RIGHT_MODULE_STEER_OFFSET -Deg2Rad(0.0)
-    #define FRONT_RIGHT_MODULE_ENCODER_PORT 1
-
-    #define BACK_LEFT_MODULE_DRIVE_MOTOR 41
-    #define BACK_LEFT_MODULE_STEER_MOTOR 36
-    //#define BACK_LEFT_MODULE_STEER_OFFSET -Deg2Rad(0.0)
-    #define BACK_LEFT_MODULE_ENCODER_PORT 2
-
-    #define BACK_RIGHT_MODULE_DRIVE_MOTOR 42
-    #define BACK_RIGHT_MODULE_STEER_MOTOR 34
-    //#define BACK_RIGHT_MODULE_STEER_OFFSET -Deg2Rad(0.0)
-    #define BACK_RIGHT_MODULE_ENCODER_PORT 0
+#ifdef ROBOT_SABER
+#define LEFT_GRID_BUTTON -1 // 51
+#define MIDDLE_GRID_BUTTON -1 // 52
+#define RIGHT_GRID_BUTTON -1 // 53
+#define PLACING_BUTTON_A1 -1 //
+#define PLACING_BUTTON_A2 -1 // 
+#define PLACING_BUTTON_A3 -1 // 
+#define PLACING_BUTTON_B1 -1 // 
+#define PLACING_BUTTON_B2 -1 // 
+#define PLACING_BUTTON_B3 -1 // 
+#define PLACING_BUTTON_C1 -1 // 
+#define PLACING_BUTTON_C2 -1 // 
+#define PLACING_BUTTON_C3 -1 //
+#define DRIVE_FRONT_LEFT -1  // 31
+#define DRIVE_FRONT_RIGHT 32
+#define DRIVE_BACK_LEFT 34
+#define DRIVE_BACK_RIGHT 33
+#define FLYWHEEL_FRONT 42
+#define FLYWHEEL_BACK 41
+#define FEEDER 3
+#define FEEDER_BUTTON 2
+#define FLYWHEEL_BUTTON_BY_DIAL 4
+#define FLYWHEEL_BUTTON_BY_DISTANCE 5
+#define FLYWHEEL_DIAL -1
+#define SHOOTTIME 14
+#define TESTBUTTON -1
+#define READYSHOOT 3
+#define CLIMBPULLUP 31
+#define CLIMBPIVOT 3
 #endif
+
+#ifdef ROBOT_GEORGE
+#define DRIVE_FRONT_LEFT 53
+#define DRIVE_FRONT_RIGHT 54
+#define DRIVE_BACK_LEFT 51
+#define DRIVE_BACK_RIGHT 52
+#define FLYWHEEL_FRONT 39
+#define FLYWHEEL_BACK 38
+#define FEEDER 22
+#define FEED_BUTTON 13
+#define FLYWHEEL_BUTTON_BY_SPEED 16
+#define FLYWHEEL_BUTTON_BY_DISTANCE 14
+#define CLIMBPULLUP 30
+#define CLIMBPIVOT 15
+#define PHOTOSENSOR 3
+#define SHOOT_BUTTON 15
+#define LIMELIGHT_TOGGLE 1
+#define INTAKE_BUTTON_UP_DOWN 6
+#define INTAKE_BUTTON_INGEST 7
+#define INTAKE_BUTTON_EJECT 8
+#define INTAKE_UP_DOWN 11
+#define INTAKE_INGEST_EJECT 2
+#endif
+
+}  // namespace OperatorConstants
