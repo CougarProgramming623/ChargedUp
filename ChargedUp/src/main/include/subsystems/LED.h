f9e603297e64de3c325f085973565328e42142d0
JJ Feeney <79488287+Jfee04@users.noreply.github.com>
2023-02-27 08:39:15 -0500

    LED outline

--- ChargedUp/src/main/include/subsystems/LED.h
+++ ChargedUp/src/main/include/subsystems/LED.h
@@ -12,11 +12,21 @@ Class LED {
 
         LED();
         void LEDInit();
-        void PaintSolid(frc::Color allianceColor);
-        void PaintSolid(int R, int G, int B);
+
         void LowBatery();
 
+        void SponsorBoardSolid(frc::Color allianceColor);
+        void SponsorBoardSolid(int R, int G, int B);
 
+        void SponsorBoardFlash(frc::Color allianceColor);     
+        void SponsorBoardFlash(int R, int G, int B);     
+
+        void EyesSolid(frc::Color allianceColor);
+        void EyesSolid(int R, int G, int B);
+
+        void EyesAngry();
+        void EyesSleepy();
+        void EyeRoll();
 
     private:
 
@@ -25,5 +35,8 @@ Class LED {
         const int kSTART_TL = 0;
         const int kSTART_TR = 0;
         const int kSTART_BR = 0;
+
+        const int kSTART_EYE_1  = 0;
+        const int kSTART_EYE_2  = 0;
 
 };
