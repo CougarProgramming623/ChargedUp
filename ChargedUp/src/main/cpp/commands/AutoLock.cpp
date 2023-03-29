5230d3612e373eccaf7a51a37beef9c92e86ca23
SeungwooDY <sdyoon0701@gmail.com>
2023-03-28 20:01:02 -0400

    auto-lock start

---
+++ ChargedUp/src/main/cpp/commands/AutoLock.cpp
@@ -0,0 +1,38 @@
+#include "commands/AutoLock.h"
+#include "Robot.h"
+#include "frc/kinematics/ChassisSpeeds.h"
+
+AutoLock::AutoLock() {
+    AddRequirements(&Robot::GetRobot()->GetDriveTrain());
+}
+
+void AutoLock::Initialize(){}
+
+//If input is below deadband, set to zero
+double AutoLock::Deadfix(double in, double deadband) {
+    if(abs(in) < deadband) {
+        return 0;
+    }
+    return in;
+}
+
+//Take joystick input, convert to ChassisSpeeds object, and pass to BaseDrive() function
+void AutoLock::Execute() {    
+    Robot* r = Robot::GetRobot();
+    std::abs(r.getAngle()) > 90 ? m_Theta = 180: m_Theta = 0;
+    auto pidVal = m_ThetaController.Calculate(units::radians_t{Deg2Rad(r.getAngle())}, (m_Theta))
+    //DebugOutF(std::to_string(fmod(360 + 90 - r->GetNavX().GetAngle(), 360)));
+    frc::ChassisSpeeds speeds = frc::ChassisSpeeds::FromFieldRelativeSpeeds(
+            units::meters_per_second_t(-Deadfix(r->GetJoyStick().GetRawAxis(1), 0.02) * kMAX_VELOCITY_METERS_PER_SECOND * 0.7),
+            units::meters_per_second_t(Deadfix(r->GetJoyStick().GetRawAxis(0), 0.02) * kMAX_VELOCITY_METERS_PER_SECOND * 0.7),
+            units::radians_per_second_t(m_ThetaController(0.5, 0.5, 0.5)),
+            frc::Rotation2d(units::radian_t(Deg2Rad(-fmod(360 - r->GetNavX().GetAngle(), 360))))
+    );
+
+    // if(COB_GET_ENTRY(COB_KEY_IS_RED).GetBoolean(false)){
+    //     speeds.vx = -speeds.vx;
+    //     speeds.vy = -speeds.vy;
+    // }
+    
+    r->GetDriveTrain().BaseDrive(speeds);
+}
---
+++ ChargedUp/src/main/include/commands/AutoLock.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <frc2/command/CommandBase.h>
+#include <frc2/command/CommandHelper.h>
+#include <frc2/command/SubsystemBase.h>
+
+
+class AutoLock : public frc2::CommandHelper<frc2::CommandBase, AutoLock> {
+ public:
+  explicit AutoLock();
+  //~DriveWithJoystick();
+
+  void Initialize() override;
+  void Execute() override;
+  double Deadfix(double in, double deadband);
+
+  int m_Theta;
+  frc::ProfiledPIDController <units::radians> m_ThetaController;
+};
