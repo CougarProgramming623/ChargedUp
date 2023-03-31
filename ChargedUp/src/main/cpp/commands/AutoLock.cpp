72c621380c6f23820dacce68c7dcfb7d61bf91dd
Chris <dermentzoglou.chris@gmail.com>
2023-03-31 10:35:00 -0400

    builds

--- ChargedUp/src/main/cpp/commands/AutoLock.cpp
+++ ChargedUp/src/main/cpp/commands/AutoLock.cpp
@@ -20,19 +20,14 @@ double AutoLock::Deadfix(double in, double deadband) {
 //Take joystick input, convert to ChassisSpeeds object, and pass to BaseDrive() function
 void AutoLock::Execute() {    
     Robot* r = Robot::GetRobot();
-    std::abs(r.getAngle()) > 90 ? m_GoalTheta = 180: m_GoalTheta = 0;
+    std::abs(r->GetAngle()) > 90 ? m_GoalTheta = 180: m_GoalTheta = 0;
     //DebugOutF(std::to_string(fmod(360 + 90 - r->GetNavX().GetAngle(), 360)));
     frc::ChassisSpeeds speeds = frc::ChassisSpeeds::FromFieldRelativeSpeeds(
-            units::meters_per_second_t(-Deadfix(r->GetJoyStick().GetRawAxis(1), 0.02) * kMAX_VELOCITY_METERS_PER_SECOND * 0.7),
-            units::meters_per_second_t(Deadfix(r->GetJoyStick().GetRawAxis(0), 0.02) * kMAX_VELOCITY_METERS_PER_SECOND * 0.7),
-            units::radians_per_second_t(r->GetDriveTrain().m_ThetaController.Calculate(units::radians_t{Deg2Rad(r.getAngle())}, m_GoalTheta)),
+            units::meters_per_second_t(-Deadfix(r->GetJoyStick().GetRawAxis(1), 0.02) * r->GetDriveTrain().kMAX_VELOCITY_METERS_PER_SECOND * 0.7),
+            units::meters_per_second_t(Deadfix(r->GetJoyStick().GetRawAxis(0), 0.02) * r->GetDriveTrain().kMAX_VELOCITY_METERS_PER_SECOND * 0.7),
+            units::radians_per_second_t(r->GetDriveTrain().GetHolonomicController().Calculate(r->GetDriveTrain().GetOdometry()->GetEstimatedPosition(), frc::Pose2d(0_m, 0_m, units::radian_t(m_GoalTheta)), 0_m / 1_s, units::radian_t(m_GoalTheta)).omega()),
             frc::Rotation2d(units::radian_t(Deg2Rad(-fmod(360 - r->GetNavX().GetAngle(), 360))))
     );
-
-    // if(COB_GET_ENTRY(COB_KEY_IS_RED).GetBoolean(false)){
-    //     speeds.vx = -speeds.vx;
-    //     speeds.vy = -speeds.vy;
-    // }
     
     r->GetDriveTrain().BaseDrive(speeds);
 }
